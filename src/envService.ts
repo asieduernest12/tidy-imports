80497ad293f1f9062b58b08b47fc4767f9822010
linuxdev <asieduernest@yahoo.co.uk>
2024-11-29 16:10:49 -0500

    Update: default envs

--- src/envService.ts
+++ src/envService.ts
@@ -2,7 +2,7 @@ import z from "zod";
 
 const envs = z
     .object({
-        NODE_ENV: z.enum(["local", "production", "staging", "development"]).default("local"),
+        NODE_ENV: z.enum(["local", "production", "staging", "development"]).default("production"),
     })
     .parse({ NODE_ENV: process.env.NODE_ENV });
 
