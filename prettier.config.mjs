4cb47fc1757806e90a163e3a22378fe9e6caacfa
linuxdev <asieduernest@yahoo.co.uk>
2024-11-29 15:08:54 -0500

    fix: rollup build and add bin for npx in packjson

---
+++ prettier.config.mjs
@@ -0,0 +1,17 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+export default {
+    parser: "babel",
+    printWidth: 140,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    singleQuote: false,
+    trailingComma: "es5",
+    bracketSpacing: true,
+    arrowParens: "always",
+    rangeStart: 0,
+    plugins: ["prettier-plugin-sort-imports", "prettier-plugin-organize-imports"],
+};
---
+++ src/envService.ts
@@ -0,0 +1,9 @@
+import z from "zod";
+
+const envs = z
+    .object({
+        NODE_ENV: z.enum(["local", "production", "staging", "development"]).default("local"),
+    })
+    .parse({ NODE_ENV: process.env.NODE_ENV });
+
+export default { envs };
